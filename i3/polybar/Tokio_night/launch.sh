2d3b87edfabd2a36b23faceb65b7ce074fe9efc3
igorjoxa1118 <igorskorolevs5@gmail.com>
2023-12-06 21:29:01 +0200

    updated/added  M i3/polybar/Tokio_night/colors/polybar/colors.ini  M i3/polybar/Tokio_night/config.ini  M i3/polybar/Tokio_night/launch.sh  M i3/polybar/Tokio_night/modules  M i3/rofi/themes/colors.rasi  M i3/rofi/themes/network.rasi ?? i3/rofi/themes/networkmanager.rasi

--- i3/polybar/Tokio_night/launch.sh
+++ i3/polybar/Tokio_night/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/Документы/dotfiles/config/polybar/config
+# Launch Polybar
 polybar top --config=~/.config/i3/polybar/Tokio_night/config.ini &
 polybar bottom --config=~/.config/i3/polybar/Tokio_night/config.ini &
 
