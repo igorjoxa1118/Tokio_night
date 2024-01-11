bbdf055e6c6f9d36a2cb6ae8dd905d5cc23841f4
igorjoxa1118 <igorskorolevs5@gmail.com>
2024-01-11 12:19:52 +0200

    updated/added  M user/.config/i3/polybar/Tokio_night/colors/polybar/colors.ini  M user/.config/i3/polybar/Tokio_night/modules  M user/.config/i3/scripts/stalonetray.sh  ? user/.config/ranger/plugins/ranger-cmus

--- user/.config/i3/scripts/stalonetray.sh
+++ user/.config/i3/scripts/stalonetray.sh
@@ -6,12 +6,12 @@ config="$HOME/.config/stalonetray/stalonetrayrc"
 
 if pgrep -x "stalonetray" > /dev/null
 then
-    polybar-msg action "#systray.hook.1"
+    polybar-msg action "#systray.hook.0"
     touch "$hidden"
     killall stalonetray
     sed -i 's/systray\ninitial=.*/systray\ninitial=2/g' "$modules"
 else
-    polybar-msg action "#systray.hook.0"
+    polybar-msg action "#systray.hook.1"
     rm "$hidden"
     stalonetray --config $config
     sed -i 's/systray\ninitial=.*/systray\ninitial=1/g' "$modules"
