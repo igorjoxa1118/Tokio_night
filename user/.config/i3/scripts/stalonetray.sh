507e67fb1de62133c09e930c0510be449df361bd
igorjoxa1118 <igorskorolevs5@gmail.com>
2024-01-11 11:58:18 +0200

    updated/added  M README.md  M user/.config/i3/keybinds  M user/.config/i3/polybar/Tokio_night/config.ini  M user/.config/i3/polybar/Tokio_night/modules  ? user/.config/ranger/plugins/ranger-cmus ?? user/.config/i3/scripts/stalonetray.sh ?? user/.config/stalonetray/

---
+++ user/.config/i3/scripts/stalonetray.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+hidden=/tmp/syshide.lock
+modules="$HOME/.config/i3/polybar/Tokio_night/modules"
+config="$HOME/.config/stalonetray/stalonetrayrc"
+
+if pgrep -x "stalonetray" > /dev/null
+then
+    polybar-msg action "#systray.hook.1"
+    touch "$hidden"
+    killall stalonetray
+    sed -i 's/systray\ninitial=.*/systray\ninitial=2/g' "$modules"
+else
+    polybar-msg action "#systray.hook.0"
+    rm "$hidden"
+    stalonetray --config $config
+    sed -i 's/systray\ninitial=.*/systray\ninitial=1/g' "$modules"
+fi
