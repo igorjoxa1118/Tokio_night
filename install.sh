63330de6a78db46d5f96af049fb9edde3c4a3b0a
igorjoxa1118 <igorskorolevs5@gmail.com>
2024-02-14 16:49:51 +0200

    updated/added  M install.sh

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ clear
 
 dependencias=(base-devel yad cmus rsync mpv jq git socat mpd polkit-gnome stalonetray kitty lsd ranger \
               micro blueman mousepad ristretto firefox thunar thunar-volman thunar-media-tags-plugin thunar-archive-plugin \
-              caja polybar rofi dunst xdg-user-dirs nitrogen picom yt-dlp \
+              caja polybar rofi dunst xdg-user-dirs engrampa nitrogen picom yt-dlp \
               fzf mcfly neofetch zsh zsh-syntax-highlighting zsh-autosuggestions \
               zsh-history-substring-search starship bluez-utils bluez-tools bluez-plugins bluez-libs bluez blueman)
 
